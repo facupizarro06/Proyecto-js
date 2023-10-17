9771d67c5cbd1d9c7ab13cf7bc72bf2b73adfc9d
Facundo Pizarro <facupizarrobmx@gmail.com>
2023-10-17 16:51:17 -0300

    funciones js

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function agregarAlCarrito(productoId) {
     const producto = productos.find(p => p.id === productoId);
 
     if (producto) {
-        carrito.push(producto);
+        const productoEnCarrito = carrito.find(item => item.id === productoId);
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++;
+        } else {
+            producto.cantidad = 1;
+            carrito.push(producto);
+        }
+        
         localStorage.setItem('carrito', JSON.stringify(carrito));
         mostrarCarrito();
     }
@@ -32,9 +39,14 @@ function mostrarCarrito() {
     let total = 0;
     carrito.forEach(producto => {
         const itemCarrito = document.createElement('li');
-        itemCarrito.innerHTML = `${producto.nombre} - $${producto.precio} <button class="eliminar" data-id="${producto.id}">Eliminar</button>`;
+        itemCarrito.innerHTML = `
+            ${producto.nombre} - $${producto.precio} x ${producto.cantidad}
+            <button class="eliminar" data-id="${producto.id}">Eliminar</button>
+            <button class="aumentar" data-id="${producto.id}">+</button>
+            <button class="disminuir" data-id="${producto.id}">-</button>
+        `;
         listaCarrito.appendChild(itemCarrito);
-        total += producto.precio;
+        total += producto.precio * producto.cantidad;
     });
 
     totalCarrito.textContent = `$${total.toFixed(2)}`;
@@ -53,6 +65,12 @@ listaCarrito.addEventListener('click', (e) => {
     if (e.target.classList.contains('eliminar')) {
         const productoId = parseInt(e.target.getAttribute('data-id'));
         eliminarDelCarrito(productoId);
+    } else if (e.target.classList.contains('aumentar')) {
+        const productoId = parseInt(e.target.getAttribute('data-id'));
+        aumentarCantidad(productoId);
+    } else if (e.target.classList.contains('disminuir')) {
+        const productoId = parseInt(e.target.getAttribute('data-id'));
+        disminuirCantidad(productoId);
     }
 });
 
@@ -62,10 +80,43 @@ vaciarCarritoBtn.addEventListener('click', () => {
     mostrarCarrito();
 });
 
+// Función para aumentar la cantidad de un producto en el carrito
+function aumentarCantidad(productoId) {
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    const productoEnCarrito = carrito.find(item => item.id === productoId);
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        mostrarCarrito();
+    }
+}
+
+// Función para disminuir la cantidad de un producto en el carrito
+function disminuirCantidad(productoId) {
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    const productoEnCarrito = carrito.find(item => item.id === productoId);
+    if (productoEnCarrito && productoEnCarrito.cantidad > 1) {
+        productoEnCarrito.cantidad--;
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        mostrarCarrito();
+    }
+}
+// Función para finalizar la compra
+function finalizarCompra() {
+    const ordenCompra = Math.floor(Math.random() * 1000000); // Generar un número de orden aleatorio
+    document.getElementById('ordenCompra').textContent = `Número de Orden de Compra: #${ordenCompra}`;
+    document.getElementById('ordenCompra').style.display = 'block';
+    localStorage.removeItem('carrito'); // Limpiar el carrito al finalizar la compra
+    mostrarCarrito();
+}
+
+// Evento para finalizar la compra
+document.getElementById('finalizarCompra').addEventListener('click', finalizarCompra);
+
 // Datos de ejemplo de productos (puedes modificarlos o cargarlos desde una fuente externa)
 const productos = [
-    { id: 1, nombre: 'Adidas Falcon', precio: 50 },
-    { id: 2, nombre: 'Nike Total White', precio: 60 },
+    { id: 1, nombre: 'Adidas Falcon', precio: 5000 },
+    { id: 2, nombre: 'Nike Air Force Total White', precio: 6000 },
     // Agrega más productos aquí
 ];
 
